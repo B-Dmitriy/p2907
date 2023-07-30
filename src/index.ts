e84da9b4c34119af40c2573490fb14d45bce86d5
BDmitriy <balyaevds.main@gmail.com>
2023-07-30 16:12:00 +0300

    init database

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,36 @@
 import express, {Request, Response} from 'express'
+import pgPromise, { IMain } from 'pg-promise'
+
 const app = express()
 const port = 3000
+const pgp = pgPromise()
+const db = pgp("postgres://postgres:postgres@localhost:5432/postgres")
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello Dima!!!')
-})
+app.get('/', function (req: Request, res: Response) {
+  
+  db.any("SELECT * FROM todos")
+    .then(function (data) {
+        res.send(data);
+    })
+    .catch(function (error) {
+      res.send("Error: " + error);
+    });
+});
+
+app.post('/', function (req, res) {
+  res.send('Got a POST request');
+});
+
+
+app.put('/todos', function (req, res) {
+  res.send('Got a PUT request at /todos');
+});
+
+
+app.delete('/todos', function (req, res) {
+  res.send('Got a DELETE request at /todos');
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
