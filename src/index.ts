4cce663b7a82f1ed7065f8b3038ff2d7c9c2778a
BDmitriy <balyaevds.main@gmail.com>
2023-08-06 16:21:34 +0300

    add tests and refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import { app } from './app/app';
+import { db } from './config/database';
 
 const port = process.env.PORT || 7000;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+const databaseReady = db.$pool.connect();
+
+databaseReady
+  .then(() => {
+    console.log("Database connection comlete");
+
+    try {
+      app.listen(port, () => {
+        console.log(`Server start on port ${port}`);
+      })
+    } catch (err) {
+      console.log("Server start error: ", err);
+    }
+  })
+  .catch((err) => console.log(err));
 
