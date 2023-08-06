1d11c87f2bc1bea21812b93c7304de730ed3117c
BDmitriy <balyaevds.main@gmail.com>
2023-08-06 15:40:45 +0300

    add e2e test

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,6 @@
-import cors from 'cors';
-import express from 'express';
-import coockeyParser from 'cookie-parser';
-import { authRouter } from './routes/authRouter';
-import { todosRouter } from './routes/todosRouter';
-import 'dotenv/config';
+import { app } from './app/app';
 
-const app = express();
 const port = process.env.PORT || 7000;
-
-app.use(express.json());
-app.use(cors());
-app.use(coockeyParser());
-
-app.use('/auth', authRouter);
-app.use('/todos', todosRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
---
+++ test/e2e/todosAPI.test.ts
@@ -0,0 +1,21 @@
+import request from 'supertest';
+import { app } from '../../src/app/app';
+import { db } from '../../src/config/database';
+
+describe('/todos', () => {
+    afterAll(() => {
+        db.$pool.end()
+    });
+
+    it('should be 200', async () => {
+        await request(app)
+            .get('/todos?userId=1')
+            .expect(200)
+    })
+
+    it('should be 400', async () => {
+        await request(app)
+            .get('/todos')
+            .expect(400)
+    })
+});
