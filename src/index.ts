535c07185fed65279575651622f59e8acff31fc7
BDmitriy <balyaevds.main@gmail.com>
2023-08-06 14:36:25 +0300

    add cors *

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+import cors from 'cors';
 import express from 'express';
+import coockeyParser from 'cookie-parser';
 import { authRouter } from './routes/authRouter';
 import { todosRouter } from './routes/todosRouter';
 import 'dotenv/config';
@@ -7,6 +9,8 @@ const app = express();
 const port = process.env.PORT || 7000;
 
 app.use(express.json());
+app.use(cors());
+app.use(coockeyParser());
 
 app.use('/auth', authRouter);
 app.use('/todos', todosRouter);
